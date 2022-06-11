a20145b44391cc8fc6e1370842b8f4cf838bbe3c
schqiushui <orochi9999@gmail.com>
2022-06-12 04:42:27 +0800

    techpack: data: Fix remaining format-invalid-specifier warnings
    Signed-off-by: schqiushui <orochi9999@gmail.com>

--- techpack/data/rmnet/shs/rmnet_shs_wq.c
+++ techpack/data/rmnet/shs/rmnet_shs_wq.c
@@ -1226,11 +1226,12 @@ int rmnet_shs_wq_check_cpu_move_for_ep(u16 current_cpu, u16 dest_cpu,
 	cpu_in_rps_mask = (1 << dest_cpu) & ep->rps_config_msk;
 
 	rm_err("SHS_MASK:  cur cpu [%d] | dest_cpu [%d] | "
-	       "cpu isolation_mask = 0x%x | ep_rps_mask = 0x%x | "
+	       "cpu isolation_mask = 0x%lx | ep_rps_mask = 0x%x | "
 	       "cpu_online(dest) = %d cpu_in_rps_mask = %d | "
 	       "cpu isolated(dest) = %d",
-	       current_cpu, dest_cpu, __cpu_isolated_mask, ep->rps_config_msk,
-	       cpu_online(dest_cpu), cpu_in_rps_mask, cpu_isolated(dest_cpu));
+	       current_cpu, dest_cpu, *cpumask_bits(cpu_isolated_mask),
+	       ep->rps_config_msk, cpu_online(dest_cpu), cpu_in_rps_mask,
+	       cpu_isolated(dest_cpu));
 
 	/* We cannot move to dest cpu if the cur cpu is the same,
 	 * the dest cpu is offline, dest cpu is not in the rps mask,
